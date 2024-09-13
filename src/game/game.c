3f428f184d6deb3920f3af61574669fccb71940b
Aven Bross <kieroda@gmail.com>
2024-09-13 17:22:36 -0500

    Text experiments.

--- src/game/game.c
+++ src/game/game.c
@@ -12,6 +12,12 @@
 
 #include "../game.h"
 
+#define STBTT_STATIC
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb_truetype.h"
+
+#include <stdio.h>
+
 #define ROTATION_VELOCITY (2.0f * AVEN_GLM_PI_F / 10.0f)
 #define MAX_VERTICES 256
 #define MAX_INDICES 512
@@ -44,12 +50,12 @@ typedef List(GLushort) IndexList;
 static const char* vertex_shader_text =
     "#version 100\n"
     "precision mediump float;\n"
-    "uniform mat4 MVP;\n"
+    "uniform mat4 uMVP;\n"
     "attribute vec2 vTex;\n"
     "attribute vec2 vPos;\n"
     "varying vec2 texCoord;\n"
     "void main() {\n"
-    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+    "    gl_Position = uMVP * vec4(vPos, 0.0, 1.0);\n"
     "    texCoord = vTex;\n"
     "}\n";
 
@@ -59,8 +65,16 @@ static const char* fragment_shader_text =
     "uniform sampler2D texSampler;\n"
     "varying vec2 texCoord;\n"
     "void main() {\n"
-    "    gl_FragColor = texture2D(texSampler, texCoord);\n"
+    "    vec4 temp = texture2D(texSampler, texCoord);\n"
+    "    gl_FragColor = vec4(1.0, 1.0, 1.0, temp.w);\n"
     "}\n";
+
+#define ROWS ((20 + 2) * 2 * 4)
+#define COLS ((20 + 2) * 2 * 9)
+static unsigned char ttf_buffer[1<<20];
+static unsigned char temp_bitmap[ROWS * COLS];
+
+static stbtt_packedchar cdata[96]; // ASCII 32..126 is 95 glyphs
 
 static void game_load(GameCtx *ctx, AvenGL *gl) {
     gl->GenBuffers(1, &ctx->gl.vertex_buffer);
@@ -103,7 +117,7 @@ static void game_load(GameCtx *ctx, AvenGL *gl) {
 
     ctx->gl.mvp_location = (GLuint)gl->GetUniformLocation(
         ctx->gl.program,
-        "MVP"
+        "uMVP"
     );
     assert(gl->GetError() == 0);
     ctx->gl.vpos_location = (GLuint)gl->GetAttribLocation(
@@ -144,25 +158,66 @@ static void game_load(GameCtx *ctx, AvenGL *gl) {
     assert(gl->GetError() == 0);
     gl->BindTexture(GL_TEXTURE_2D, ctx->gl.tex_id);
     assert(gl->GetError() == 0);
+
+    fread(
+        ttf_buffer,
+        1,
+        1<<20,
+        fopen("/usr/share/fonts/TTF/FiraCode-Regular.ttf", "rb")
+    );
+    stbtt_pack_context pc;
+    int success = stbtt_PackBegin(
+        &pc,
+        temp_bitmap,
+        ROWS,
+        COLS,
+        0,
+        1,
+        NULL
+    );
+    assert(success != 0);
+    stbtt_PackSetOversampling(&pc, 2, 2);
+    success = stbtt_PackFontRange(
+        &pc,
+        ttf_buffer,
+        0,
+        20.0f,
+        32,
+        96,
+        cdata
+    );
+    assert(success != 0);
+    stbtt_PackEnd(&pc);
     gl->TexImage2D(
         GL_TEXTURE_2D,
         0,
-        GL_RGBA,
-        2,
-        2,
+        GL_ALPHA, 
+        ROWS,
+        COLS,
         0,
-        GL_RGBA,
+        GL_ALPHA,
         GL_UNSIGNED_BYTE,
-        texture
+        temp_bitmap
     );
+    //gl->TexImage2D(
+    //    GL_TEXTURE_2D,
+    //    0,
+    //    GL_RGBA,
+    //    2,
+    //    2,
+    //    0,
+    //    GL_RGBA,
+    //    GL_UNSIGNED_BYTE,
+    //    texture
+    //);
     //gl->GenerateMipmap(GL_TEXTURE_2D);
     gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     assert(gl->GetError() == 0);
     gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     assert(gl->GetError() == 0);
-    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     assert(gl->GetError() == 0);
-    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     assert(gl->GetError() == 0);
     gl->BindTexture(GL_TEXTURE_2D, 0);
     assert(gl->GetError() == 0);
@@ -214,6 +269,7 @@ int game_update(
     ctx->angle += ROTATION_VELOCITY * elapsed_sec;
 
     float ratio = (float)width / (float)height;
+    float pixel_size = 2.0f / (float)height;
 
     gl->Viewport(0, 0, width, height);
     assert(gl->GetError() == 0);
@@ -223,15 +279,24 @@ int game_update(
     Mat4 id_m, rot_m, per_m, mvp;
     mat4_identity(id_m);
     mat4_rotate_z(rot_m, id_m, ctx->angle);
-    mat4_ortho(per_m, -ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+    mat4_ortho(per_m, ratio, -ratio, -1.0f, 1.0f, 1.0f, -1.0f);
     mat4_mul_mat4(mvp, per_m, rot_m);
+
+    float x = 0.0f;
+    float y = 0.0f;
+    stbtt_aligned_quad q;
+    stbtt_GetPackedQuad(cdata, ROWS, COLS, '6' - 32, &x, &y, &q, 0);
+    q.x0 *= pixel_size;
+    q.x1 *= pixel_size;
+    q.y0 *= pixel_size;
+    q.y1 *= pixel_size;
 
     Vertex vertex_data[MAX_VERTICES];
     VertexList vertices = list_array(vertex_data);
-    list_push(vertices) = (Vertex){ { -0.5f, -0.5f }, { 0.0f, 0.0f, } };
-    list_push(vertices) = (Vertex){ {  0.5f, -0.5f }, { 0.0f, 4.0f, } };
-    list_push(vertices) = (Vertex){ {  0.5f,  0.5f }, { 4.0f, 4.0f, } };
-    list_push(vertices) = (Vertex){ { -0.5f,  0.5f }, { 4.0f, 0.0f, } };
+    list_push(vertices) = (Vertex){ { q.x0, q.y0, }, { q.s0, q.t0, } };
+    list_push(vertices) = (Vertex){ { q.x1, q.y0, }, { q.s1, q.t0, } };
+    list_push(vertices) = (Vertex){ { q.x1, q.y1, }, { q.s1, q.t1, } };
+    list_push(vertices) = (Vertex){ { q.x0, q.y1, }, { q.s0, q.t1, } };
 
     gl->BindBuffer(GL_ARRAY_BUFFER, ctx->gl.vertex_buffer);
     assert(gl->GetError() == 0);
@@ -259,9 +324,15 @@ int game_update(
         (GLint)ctx->gl.mvp_location,
         1,
         GL_FALSE,
-        (GLfloat*)&mvp
+        (GLfloat*)mvp
     );
     assert(gl->GetError() == 0);
+
+    gl->Enable(GL_BLEND);
+    assert(gl->GetError() == 0);
+    gl->BlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    assert(gl->GetError() == 0);
+    //gl->Enable(GL_BLEND);
     gl->BindTexture(GL_TEXTURE_2D, ctx->gl.tex_id);
     assert(gl->GetError() == 0);
     gl->DrawElements(
